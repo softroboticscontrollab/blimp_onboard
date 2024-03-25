d43a8cdf7bba4e02dab2b079d5c8dce8a9d10fdf
Andrew Sabelhaus <asabelha@bu.edu>
2024-03-24 23:26:03 -0400

    first full working draft for v0.4 active capture mechanism

--- arduino/active_capture_v04/active_capture_v04/active_capture_v04.ino
+++ arduino/active_capture_v04/active_capture_v04/active_capture_v04.ino
@@ -18,25 +18,17 @@
 
 // pin setup and constants
 // will use two PWMs for one motor
-int AIN1 = 0;
-int AIN2 = 1;
+int AIN1 = 9;
+int AIN2 = 10;
 // placeholders for what we'll read from the serial terminal
 String received;
-float duty_AIN1 = 0.0;
-float duty_AIN2 = 0.0;
 float pwr = 0.0;
 int motor_runtime_millis = 0;
 int raw_duty = 0;
-float highlow_inv_duty = 0.0;
 // because compiler is annoying and sscanf can't do floats directly
 char *token;
 char received_raw[BUFSIZE];
 String cmd_letter = "";
-// for a circuit that switches high-to-low, such as using BJTs instead of MOSFETS to control a robot
-bool switch_highlow = true;
-// Sensor reading periods. Example here with two sensors. In milliseconds.
-// unsigned long sper1 = 500; // good number for the BendLabs One Axis sensor running over I2C at 400 Hz (kHz?) is 20 msec
-// unsigned long sper2 = 1200; // the temperature sensor (MCP9600) samples much more slowly, even when setting the resolution low for its fastest readings is 100 msec
 // Time management
 unsigned long curr_time;
 unsigned long prev_time;
@@ -45,12 +37,7 @@ bool motor_is_on = 0;
 // helper function to turn a 0.0-1.0 float into an 8-bit duty cycle
 int convert_duty(float duty)
 {
-  // If we're using the inverting BJT circuit, duty cycle is "opposite."
-  highlow_inv_duty = duty;
-  if(switch_highlow){
-    highlow_inv_duty = 1.0 - highlow_inv_duty;
-  }
-  raw_duty = floor(highlow_inv_duty * MAX_CC);
+  raw_duty = floor(duty * MAX_CC);
   // constrain between 0 and MAX_CC.
   raw_duty = (raw_duty < 0) ? 0 : raw_duty;
   raw_duty = (raw_duty > MAX_CC) ? MAX_CC : raw_duty;
@@ -72,7 +59,7 @@ void setup() {
   // we'll also turn on the LED when any pin is set high
   pinMode(LED_BUILTIN, OUTPUT);
   digitalWrite(LED_BUILTIN, LOW);
-  // Time management for sensor sampling:
+  // Time management for timed motor loops
   prev_time = millis();
   curr_time = millis();
 }
@@ -80,7 +67,7 @@ void setup() {
 void loop() {
   // If there is at least one byte in the serial buffer, someone is trying to send us data, so read that first (don't lose anything!)
   if( Serial.available() > 0){
-    // presummably the user is smart enough to send the two floats... TODO validate the received string!
+    // presummably the user is smart enough to send the right string... TODO validate!
     received = Serial.readString();
     // using strtok to parse manually
     received.toCharArray(received_raw, BUFSIZE);
@@ -98,30 +85,35 @@ void loop() {
       token = strtok(NULL, " ");
       motor_runtime_millis = int(atof(token)*1000.0);
     }
+    //////////////////
+    // DEBUGGING
     Serial.println("You want to run the motor ");
     Serial.println(cmd_letter);
     Serial.println("At this power:");
     Serial.println(pwr);
     Serial.println("For this many milliseconds:");
     Serial.println(motor_runtime_millis);
+    // END DEBUGGING
+    //////////////////
     // If we got a good reading, set up for a timed loop:
     if(cmd_letter == "f" or cmd_letter == "r"){
-      // Serial.println("Valid command.");
       // Reset the time
       prev_time = millis();
       curr_time = millis();
       // We choose fast decay here ("coast"). So, per https://www.ti.com/lit/ds/symlink/drv8833.pdf, ...
       if(cmd_letter == "f"){
         // if forward, AIN1 is PWM, and AIN2 is low.
+        Serial.println("Turning motor ON FWD");
         analogWrite(AIN1, convert_duty(pwr));
+        // digitalWrite(AIN1, 1);
         analogWrite(AIN2, 0);
       }
-      if(cmd_letter = "r"){
+      if(cmd_letter == "r"){
         // if reverse, AIN1 is low, and AIN2 is PWM.
+        Serial.println("Turning motor ON REV");
         analogWrite(AIN1, 0);
         analogWrite(AIN2, convert_duty(pwr));
       }
-      Serial.println("Turning motor ON");
       motor_is_on = 1;
       digitalWrite(LED_BUILTIN, HIGH);
       // run until time is up...
@@ -141,51 +133,4 @@ void loop() {
       digitalWrite(LED_BUILTIN, LOW);
     }
   }
-}
-
-//   // Next, sensor readings. If it's past the interval for either one, take a reading, and send.
-//   curr_time = millis();
-//   if( (curr_time - prev_time) > sper1){
-//     sdata1 = read_sensor1();
-//     prev_time = curr_time;
-//     // Format and send
-//     datatx(sdata1, sprefix1);
-//   }
-//   if( (curr_time - prev_time2) > sper2){
-//     sdata2 = read_sensor2();
-//     prev_time2 = curr_time;
-//     // Format and send
-//     datatx(sdata2, sprefix2);
-//   }
-// }
-
-// String read_sensor1()
-// {
-//   dat1[0] = 6.5;
-//   // do something interesting for a test
-//   for(int i=0; i < n_sens1; i++){
-//     dat1[i] = dat1[i] + (random(-100,100)/10.0);
-//   }
-//   return fmt_dat(dat1, n_sens1); 
-// }
-
-// String read_sensor2()
-// {
-//   dat2[0] = 2.7;
-//   dat2[1] = 78.4;
-//   // do something interesting for a test
-//   for(int i=0; i < n_sens2; i++){
-//     dat2[i] = dat2[i] + (random(-100,100)/10.0);
-//   }
-//   return fmt_dat(dat2, n_sens2); 
-// }
-
-String fmt_dat(float dat[], int nsens)
-{
-  // format the floats into a space-separated string
-  String res = "";
-  for(int i=0; i<nsens; i++){
-    res = res + String(dat[i]) + " ";
-  }
-  return res;
 }
